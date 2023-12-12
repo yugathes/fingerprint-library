d5fbc490876e1e16174b7657fc18fb9ecddd8420
Yugathes10 <yugathes@trinoviklabs.com>
2023-12-13 01:39:57 +0800

    Remove unused input

--- Web/Lecturer/Menu.php
+++ Web/Lecturer/Menu.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 //if session exists
-if(isset ($_SESSION["username"])) //call this function to check if session exists or not
+if(isset ($_SESSION["userId"])) //call this function to check if session exists or not
 {
 	include "Header.php";?>
 <!DOCTYPE html>
@@ -10,7 +10,7 @@ if(isset ($_SESSION["username"])) //call this function to check if session exist
 
 <body>
 <?php
-	$queryGet = "select * from users where username='".$_SESSION["username"]."'";
+	$queryGet = "select * from users where id ='".$_SESSION["userId"]."'";
 
 	$resultGet = mysqli_query($link,$queryGet);
 
@@ -23,22 +23,20 @@ if(isset ($_SESSION["username"])) //call this function to check if session exist
 	<div class="wrapper">
 		<div class="middle">
 			<div class="contentnew" style="margin-bottom: 50px;">
-			<form class = "content" style="position: relative; width: 60%; margin:auto;left:0" action="../Admin/StudentUpdate.php" name="EditForm" method="POST">
+			<form class = "content" style="position: relative; width: 60%; margin:auto;left:0" action="../Admin/Update.php" name="EditForm" method="POST">
 				<h1 class="header">My Profile</h1>
 				<?php	while($baris= mysqli_fetch_array($resultGet, MYSQLI_BOTH))	{?>
 					<div class="input-group">
-						<label>Username</label>
-						<input type="text" name="username" value="<?php echo $baris['username']; ?>" disabled><br><br>
-						<input type="hidden" name="username" value="<?php echo $baris['username']; ?>">
-						<input type="hidden" name="from" value="Lecturer">
+						<label>Staff ID</label>
+						<input type="text" name="staffId" value="<?php echo $baris['staffId']; ?>" disabled><br><br>
+						<input type="hidden" name="staffId" value="<?php echo $baris['staffId']; ?>"><br><br>
+						<input type="hidden" name="id" value="<?php echo $baris['id']; ?>">
 						<label>Name</label>
 						<input type="text" name="name" value="<?php echo $baris['name'] ?>"><br><br>
-						<label>Student ID</label>
-						<input type="text" name="id" value="<?php echo $baris['id'] ?>"><br><br>
-						<label>Email</label>
+                        <label>Email</label>
 						<input type="email" name="email" value="<?php echo $baris['email'] ?>"><br><br>
-						<label>Contact Number</label>
-						<input type="text" name="Hp" value="<?php echo $baris['Hp']; ?>"><br><br>
+                        <input type="hidden" name="redirect" value="../Lecturer/Menu.php">
+                        <input type="hidden" name="type_user" value="<?php echo $baris['type_user'] ?>">
 						<button style="position: relative;left: 80%"; type="submit" class="btn" name="staff">Update</button>
 					</div> <?php	}?>
 			</form>
