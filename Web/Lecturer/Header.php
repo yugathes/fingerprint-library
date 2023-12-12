a889ef48d1f16ab044a13c7846e90ba3e121ab4b
Yugathes10 <yugathes@trinoviklabs.com>
2023-12-13 01:39:07 +0800

    Remove unused nav

--- Web/Lecturer/Header.php
+++ Web/Lecturer/Header.php
@@ -6,12 +6,10 @@
     <link rel="stylesheet" type="text/css" href="../CSS/topNav.css">
 	<div class="topnav">
         <a <?php if (basename($_SERVER['PHP_SELF']) == "Menu.php") echo "class='active'"?> href="Menu.php">Menu</a>
-		<a <?php if (basename($_SERVER['PHP_SELF']) == "Class.php") echo "class='active'"?>href="Class.php">Class</a>
-		<a <?php if (basename($_SERVER['PHP_SELF']) == "Students.php") echo "class='active'"?>href="Students.php">Students</a>
 		<a <?php if (basename($_SERVER['PHP_SELF']) == "Attendance.php") echo "class='active'"?>href="Attendance.php">Attendance</a>
 	</div>
 	<div class="auth">
-<?php if(isset ($_SESSION["username"])) //session userid gets value from text field named userid, shown in user.php
+<?php if(isset ($_SESSION["userId"])) //session userid gets value from text field named userid, shown in user.php
     {?>
 	    <a style="background-color:#6f7378;" href="../Auth/Logout.php"> Logout </a>
 <?php	
@@ -24,7 +22,7 @@
 	}?>
 		</div>
 </head>
-<h4>Hi user, <?php echo $_SESSION["username"];?></h4>
+<h4>Hi user, <?php echo $_SESSION["name"];?></h4>
 <div class="left-sidebar">
 <?php //include 'count.php';?>
 </div>
