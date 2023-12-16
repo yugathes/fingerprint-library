cf3e285d3eb0781b2046c34a2ea1e5df7e7760f7
FingerprintPi <yugathesyuga@gmail.com>
2023-12-16 16:23:49 +0800

    Header fixing

--- Web/Auth/Logout.php
+++ Web/Auth/Logout.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
 if (isset($_SESSION["userId"]))
-	{
-		session_destroy();
-		echo "You have successfully logged out.";
-		header('location: Login.php');
-	}
+{
+	session_destroy();
+	header('location: Login.php');
+}
 else
 	echo " No session exists or session is expired. Please log in again <a href='Login.php'> here </a>";
 ?>
