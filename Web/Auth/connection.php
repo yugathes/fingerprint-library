1e5144bfd8162e2f332491c00cce22bc29ba2deb
Pi Library <yugathesyuga@gmail.com>
2024-01-16 23:19:01 +0800

    update connection

--- Web/Auth/connection.php
+++ Web/Auth/connection.php
@@ -1,12 +1,12 @@
 <?php
-$link=mysqli_connect('localhost', 'root', '', 'library');
+$link=mysqli_connect('localhost', 'root', 'sudo', 'library');
 // Check connection
 if (mysqli_connect_errno())
 {
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
 //      die();
 }
-$connection = new mysqli("localhost","root","","library");
+$connection = new mysqli("localhost","root","sudo","library");
 if ($connection->connect_error) {
 	die("Connection failed: " . $connection->connect_error);
 }
