d3808988fc0ea6c3355926e83352e9c353a3d123
Yugathes <yugathes@trinoviklabs.com>
2024-01-24 20:59:39 +0800

    Replace search bar with button

--- Web/Admin/navbar.php
+++ Web/Admin/navbar.php
@@ -10,10 +10,15 @@
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
             <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-                <div class="input-group">
-                    <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
-                    <input type="text" class="form-control" placeholder="Type here...">
-                </div>
+                <ul class="navbar-nav  justify-content-end">
+                    <?php	$link = "http://".$ip.":5000/attendance";?>
+                    <li class="nav-item d-flex align-items-center">
+                    <a href="<?php echo $link;?>" class="nav-link text-white font-weight-bold px-0">
+                        <i class="fa fa-calendar-check-o me-sm-1"></i>
+                        <span class="d-sm-inline d-none">Take Attendance</span>
+                    </a>
+                    </li>
+                </ul>
             </div>
             <ul class="navbar-nav  justify-content-end">
                 <li class="nav-item d-flex align-items-center">
